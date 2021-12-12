27410e48b08319b33951cd04d01ef9493a57950e
André Luiz da Silva <andreluizmicro@gmail.com>
2021-12-12 15:18:51 -0300

    * created category listing method * created validade category by name

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlredyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlredyExists) {
+    return response.status(400).json({ error: "Category alredy exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const list = categoriesRepository.list();
+
+  return response.json(list);
 });
 
 export { categoriesRoutes };
