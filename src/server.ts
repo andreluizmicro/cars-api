3dee50224fc389404f05e49e355318a768ef8035
André Luiz da Silva <andreluizmicro@gmail.com>
2021-12-12 13:49:03 -0300

    Created base structure

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "I'm alive!" });
+});
+
+app.listen(3333, () => console.log("Server is running!"));
