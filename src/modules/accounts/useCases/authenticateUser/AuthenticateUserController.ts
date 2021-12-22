2bd745fdd8e638b2c99800880f4b276acc42ffb7
André Luiz da Silva <andreluizmicro@gmail.com>
2021-12-22 07:12:58 -0300

    Created authenticate using jwt

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { AuthenticateUserUseCase } from "./AuthenticateUserUserCase";
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { password, email } = request.body;
+
+    console.log(request.body);
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    const token = await authenticateUserUseCase.execute({
+      password,
+      email,
+    });
+
+    return response.json(token);
+  }
+}
+
+export { AuthenticateUserController };
